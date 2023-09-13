19ef90bb01b041b16cac5ce85f61b168c6c6abac
aswathi <aswathimohandas21@gmail.com>
2023-09-13 23:17:28 +0530

    cal

---
+++ calcu.js
@@ -0,0 +1,12 @@
+function displaydata(data){
+    result.value+=data
+}
+function allclr(){
+    result.value=" "
+}
+function findOut(){
+         result.value=eval(result.value)
+}
+function backSpace(){
+    result.value=result.value.slice(0,-1)
+}
